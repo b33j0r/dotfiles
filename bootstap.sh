5faf0ab0ff2abf7dfa33f4cd8533b50233a14943
Brian Jorgensen <brian.jorgensen@gmail.com>
2018-02-07 12:11:42 -0700

    Update bootstrap.sh

--- bootstap.sh
+++ bootstap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 export DOTFILES_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "BOOTSTRAP uncomment stuff if nothing is happening!"
@@ -7,12 +13,13 @@ echo "BOOTSTRAP uncomment stuff if nothing is happening!"
 # source ${DOTFILES_HOME}/brew/bootstrap.sh
 # source ${DOTFILES_HOME}/common/bootstrap.sh
 # source ${DOTFILES_HOME}/macos/bootstrap.sh
-source ${DOTFILES_HOME}/fish/bootstrap.sh
-source ${DOTFILES_HOME}/tmux/bootstrap.sh
+# source ${DOTFILES_HOME}/fish/bootstrap.sh
+# source ${DOTFILES_HOME}/tmux/bootstrap.sh
 # source ${DOTFILES_HOME}/vim/bootstrap.sh
-source ${DOTFILES_HOME}/python/bootstrap.sh
+# source ${DOTFILES_HOME}/python/bootstrap.sh
 # source ${DOTFILES_HOME}/node/bootstrap.sh
-source ${DOTFILES_HOME}/misc/bootstrap.sh
+# source ${DOTFILES_HOME}/misc/bootstrap.sh
+
 if [ ! -e "${DOTFILES_HOME}/private/bootstrap.sh" ]; then
   source ${DOTFILES_HOME}/private/bootstrap.sh
 fi
