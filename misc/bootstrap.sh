a678eb5da5cf4a45a77fbc86bd66b2154cf120e7
Brian Jorgensen <brian.jorgensen@gmail.com>
2018-02-07 12:10:56 -0700

    Disable iTerm quit warning

--- misc/bootstrap.sh
+++ misc/bootstrap.sh
@@ -21,4 +21,8 @@ ln -s -f "${ITERM2_PROFILE_DIR_DOTFILES}"/* "${ITERM2_PROFILE_DIR_HOME}"/
 
 defaults write com.googlecode.iterm2 "Default Bookmark Guid" '4A44A8BC-9562-477E-B459-314793700D63'
 
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+
 popd
