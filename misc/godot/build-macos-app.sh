1ec7a09c5b3a5e2c73c3608ca34732e441629494
Brian Jorgensen <brian.jorgensen@gmail.com>
2019-07-07 14:21:56 -0700

    Add godot build script

---
+++ misc/godot/build-macos-app.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+GODOT_SRC=${HOME}/src/godot
+GODOT_INSTALL_DIR=${HOME}/Applications
+GODOT_APP=${GODOT_INSTALL_DIR}/Godot.app
+
+set -e
+
+pushd ${GODOT_SRC}
+
+scons platform=osx target=release_debug
+cp -r ./misc/dist/osx_tools.app ${GODOT_APP}
+
+mkdir -p ${GODOT_APP}/Contents/MacOS
+
+cp ./bin/godot.osx.opt.tools.64 ${GODOT_APP}/Contents/MacOS/Godot
+chmod +x ${GODOT_APP}/Contents/MacOS/Godot
+
+popd
